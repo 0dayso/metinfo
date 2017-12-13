38a2917b157ba94169226afbd7410434de7b7021
130liudingbang <130liudingbang@163.com>
2017-12-13 16:26:53 +0800

    201465

---
+++ cache/online_cn.inc.php
@@ -0,0 +1,15 @@
+<?php
+ return array (
+  0 => 
+  array (
+    'id' => '1',
+    'name' => '',
+    'no_order' => '1',
+    'qq' => '348468810',
+    'msn' => '',
+    'taobao' => '',
+    'alibaba' => '',
+    'skype' => '',
+    'lang' => 'cn',
+  ),
+); ?>
